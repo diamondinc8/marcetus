cfe3a853147e0e1d9e7df360409f7f7e932f7f2d
diamondinc__ <diamondinc7@bk.ru>
2025-06-03 22:07:52 +0300

    Реализовано удаление из корзины заказанных товаров

--- app/Http/Controllers/Products/OrderController.php
+++ app/Http/Controllers/Products/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Products;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Products\BaseController;
 use App\Http\Requests\OrderRequest;
+use App\Models\UserCart;
 use Illuminate\Http\Request;
 
 class OrderController extends BaseController
@@ -24,6 +25,8 @@ class OrderController extends BaseController
 
 
         foreach ($data['product_id'] as $product_id) {
+            $cartObject = UserCart::where('user_id', $order['user_id'])->where('product_id', $product_id)->first();
+            UserCart::destroy($cartObject->id);
             $order['product_id'] = $product_id;
             $this->service->placeOrder($order);
         }
--- resources/views/shop/cart.blade.php
+++ resources/views/shop/cart.blade.php
@@ -6,45 +6,55 @@
 
 @section('content')
     @csrf
-    @php
-        $sum = 0;
-    @endphp
-    @foreach ($cartList as $cart)
-        @php
-            $product = $products->find($cart->product_id);
-            $sum += $product->cost;
-        @endphp
-        <form action="{{ route('order.place') }}" method="POST">
-            @csrf
-            <div class="row g-0 text-center">
-                <div class="col-sm-6 col-md-8">
-                    <table class="table mt-3">
-                        <tbody>
-                            <tr>
-                                <th scope="row">
-                                    <input type="hidden" name="user_id" value={{ $user->id }}>
-                                    <input type="hidden" name="status" value="Создан">
-                                    <input type="hidden" name="adress" value="{{ $user->address }}">
-                                    <input class="form-check-input" type="checkbox" name="product_id[]"
-                                        value={{ $cart->product_id }} @checked(true)>
-                                </th>
-                                <td>{{ $products->find($cart->product_id)->title }}</td>
-                                <td>ЗДЕСЬ СДЕЛАТЬ КОЛИЧЕСТВО</td>
-                                <td>{{ $products->find($cart->product_id)->cost }}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div class="col-6 col-md-4">
+    <form action="{{ route('order.place') }}" method="POST">
+        @csrf
+        <div class="row g-0 text-center">
+            <div class="col-sm-6 col-md-8">
+                @php
+                    $sum = 0;
+                @endphp
+                @if (count($cartList) === 0)
                     <div>
-                        <p>Доставка по адресу:</p>
-                        <p>{{ $user->address }}</p>
-
-                        <p>Итого: {{ $sum }}</p>
-                        <button type="sumbit" class="btn btn-primary">Заказать</button>
+                        <h3 class="text-secondary">Пока ничего не добавлено</h3>
+                        <a href="{{ route('index') }}" class="btn btn-primary">Перейти в каталог</a>
                     </div>
+                @else
+                    @foreach ($cartList as $cart)
+                        @php
+                            $product = $products->find($cart->product_id);
+                            $sum += $product->cost;
+                        @endphp
+                        <table class="table mt-3">
+                            <tbody>
+                                <tr>
+                                    <th scope="row">
+                                        <input type="hidden" name="user_id" value={{ $user->id }}>
+                                        <input type="hidden" name="status" value="Создан">
+                                        <input type="hidden" name="adress" value="{{ $user->address }}">
+                                        <input class="form-check-input" type="checkbox" name="product_id[]"
+                                            value={{ $cart->product_id }} @checked(true)>
+                                    </th>
+                                    <td>{{ $products->find($cart->product_id)->title }}</td>
+                                    <td>ЗДЕСЬ СДЕЛАТЬ КОЛИЧЕСТВО</td>
+                                    <td>{{ $products->find($cart->product_id)->cost }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    @endforeach
+                @endif
+            </div>
+            <div class="col-6 col-md-4">
+                <div>
+                    <p>Доставка по адресу:</p>
+                    <p>{{ $user->address }}</p>
+
+                    <p>Итого: {{ $sum }}</p>
+                    <button type="sumbit"
+                        class="btn btn-primary
+                    @if (count($cartList) === 0) disabled @endif
+                    ">Заказать</button>
                 </div>
             </div>
-        </form>
-    @endforeach
+        </div>
+    </form>
 @endsection
