7f5448930b1ae04844cc845bc074134b0083f148
diamondinc__ <diamondinc7@bk.ru>
2025-06-04 20:32:26 +0300

    Добавлены модели и миграции для сборки и доставки

---
+++ database/migrations/2025_06_04_171326_create_product_assemling_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_assembling_tasks', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('order_id');
+            $table->index('order_id', 'product_assembling_task_idx');
+            $table->foreign('order_id', 'product_assembling_task_fk')->on('orders')->references('id');
+
+            $table->unsignedBigInteger('assembling_task_id');
+            $table->index('assembling_task_id', 'assembling_product_task_idx');
+            $table->foreign('assembling_task_id', 'assembling_product_task_fk')->on('assembling_tasks')->references('id');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_assemling_tasks');
+    }
+};
